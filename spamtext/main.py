1656ca25e66c5abe3a578792b0551924a93f1aad
keshavtanwar57 <63180181+keshavtanwar57@users.noreply.github.com>
2021-01-12 18:52:25 +0530

    Update main.py

--- spamtext/main.py
+++ spamtext/main.py
@@ -1,5 +1,9 @@
 import pyautogui,time
+
+# it will start spamming after 10 sec
 time.sleep(10)
+
+# abc is the text file used for the spamming
 f = open("abc.txt",'r')
 for word in f:
     pyautogui.typewrite(word)
